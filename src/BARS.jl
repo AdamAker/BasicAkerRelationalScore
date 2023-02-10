c40f11003dd000fe72df43d0f1f4fe65e6f5b0b7
Adam <adamaker2@gmail.com>
2023-02-09 19:45:50 -0600

    updating BARS.jl for readability

--- src/BARS.jl
+++ src/BARS.jl
@@ -1,8 +1,9 @@
 module BARS
 
-export splitData, calcMAE, setupBARS, calcSelfBARS, calcBARS, plotTREE
+export makeDATADict, makeBARSDict, bars, plotTREE
 
 using MLJ
+using MLJDecisionTreeInterface
 using CSV
 using DataFrames
 using Plots
@@ -12,22 +13,22 @@ using Statistics
 using Random
 using Measurements
 
-function splitData(dataFrame, testTrainSplit, targetCol, featureCol)
+function makeDataDict(dataFrame, testTrainSplit)
 
     #Load feature and target
-    targets, features = dataFrame[:,targetCol], dataFrame[:,featureCol]
+    targets, features = dataFrame[:,2], dataFrame[:,1]
     features = hcat(features)
 
-    ##Calculate the median of the feature and the target
-    medianValueFeat = median(dataFrame[:,featureCol])
-    medianValueTar = median(dataFrame[:,targetCol])
+    ##Calculate the median of the target
+    medianValueFeat = median(dataFrame[:,1])
+    medianValueTar = median(dataFrame[:,2])
 
     ##Split the data into testing and training
     train, test = partition(dataFrame, testTrainSplit,rng=Random.GLOBAL_RNG,shuffle=true)
 
     ##Group the columns together
-    trainData = [ train[:,featureCol] train[:,targetCol] ]
-    testData = [ test[:,featureCol] test[:,targetCol] ]
+    trainData = [ train[:,1] train[:,2] ]
+    testData = [ test[:,1] test[:,2] ]
 
     ##Extract the training Features and Targets
     trainFeatures = trainData[:,1]
@@ -37,20 +38,42 @@ function splitData(dataFrame, testTrainSplit, targetCol, featureCol)
     testFeatures = testData[:,1]
     testTargets = testData[:,2]
 
-    DataDict = Dict("targets" => targets,
-                    "features" => features,
-                    "feature name" => propertynames(dataFrame)[featureCol],
-                    "target name" => propertynames(dataFrame)[targetCol],
-                    "median value of target" => medianValueTar,
-                    "median value of feature" => medianValueFeat,
-                    "train data" => trainData,
-                    "test data" => testData,
-                    "train features" => trainFeatures,
-                    "train targets" => trainTargets,
-                    "test features" => testFeatures,
-                    "test targets" => testTargets)
+    dataDict = Dict(:targets => targets,
+                    :features => features,
+                    :featurename => propertynames(dataFrame)[featureCol],
+                    :targetname => propertynames(dataFrame)[targetCol],
+                    :mediantarget => medianValueTar,
+                    :medianfeature => medianValueFeat,
+                    :traindata => trainData,
+                    :testdata => testData,
+                    :trainfeatures => trainFeatures,
+                    :traintargets => trainTargets,
+                    :testfeatures => testFeatures,
+                    :testtargets => testTargets)
 
-    return DataDict
+    return dataDict
+
+end 
+
+function makeDataDict(dataFrame)
+    
+    #Load feature and target
+    targets, features = dataFrame[:,2], dataFrame[:,1]
+    features = hcat(features)
+
+    ##Calculate the median of the feature and the target
+    medianValueFeat = median(dataFrame[:,1])
+    medianValueTar = median(dataFrame[:,2])
+
+    dataDict = Dict(:targets => targets,
+                    :features => features,
+                    :featurename => propertynames(dataFrame)[1],
+                    :targetname => propertynames(dataFrame)[2],
+                    :mediantarget => medianValueTar,
+                    :medianfeature => medianValueFeat)
+
+    return dataDict
+
 end
 
 function searchFields(array,fieldname,object)
@@ -79,66 +102,122 @@ function calcMAE(trueValues,predValues)
     end
 end
 
-function setupBARS(DataDict)
+function makeBARSDict(dataDict)
 
-    #Machine Learning
-    ## train regression tree
+    if haskey(dataDict,:trainfeatures)
 
-    trainFeatures = table(reshape(DataDict["train features"], length(DataDict["train features"]),1))
-    features = table(reshape(DataDict["features"], length(DataDict["features"]),1))
-    trainTargets = DataDict["train targets"]
-    targets = DataDict["targets"]
+        trainFeatures = table(reshape(dataDict[:trainfeatures], length(dataDict[:trainfeatures]),1))
+        features = table(reshape(dataDict[:features], length(dataDict[:features]),1))
+        trainTargets = dataDict[:traintargets]
+        targets = dataDict[:targets]
 
-    #Make the tree container
-    Tree = @MLJ.load DecisionTreeRegressor pkg=DecisionTree verbosity=0;
-    tree = Tree()
+        #Make the tree container
+        Tree = @MLJ.load DecisionTreeRegressor pkg=DecisionTree verbosity=0;
+        tree = Tree()
 
-    #tuning the min_purity_increase hyperparameter
-    r1 = range(tree, :merge_purity_threshold, lower=0.001, upper=1.0, scale=:log);
-    self_tuning_tree = TunedModel(model=tree,
+        #tuning the min_purity_increase hyperparameter
+        r1 = range(tree, :merge_purity_threshold, lower=0.001, upper=1.0, scale=:log);
+        self_tuning_tree = TunedModel(model=tree,
+							        tuning=Grid(resolution=10),
+							        range=r1,
+							        measure=mae);
+
+        #store the self tuning tree in a machine
+        mach = machine(self_tuning_tree, trainFeatures, trainTargets);
+
+        #fit the model
+        MLJ.fit!(mach, verbosity=2)
+
+        #print_tree(tree)
+
+        ## apply model to make predictions
+        smartPredictions = MLJ.predict(mach,features)
+
+        ##find the splits for the tree
+        splits = []
+        searchFields(splits,:featval,fitted_params(mach)[2].tree.node)
+
+        #Calculate BARS
+        naivePredictions = []
+        for i∈1:length(dataDict[:targets])
+            push!(naivePredictions, dataDict[:mediantarget])
+        end
+
+        naiveMAE = calcMAE(dataDict[:targets],naivePredictions)
+
+        smartMAE = calcMAE(dataDict[:targets],smartPredictions)
+
+        #model performance
+        SS_Res = sum((targets-smartPredictions).^2)
+        SS_Tot = sum((targets.-mean(targets)).^2)
+        R² = 1-SS_Res/SS_Tot
+        #R² = report(mach).best_history_entry[3][1]
+
+        BARSDict = Dict(:R² => R²,
+                        :smartMAE => smartMAE,
+                        :naiveMAE => naiveMAE,
+                        :naivepredictions => naivePredictions,
+                        :smartpredictions => smartPredictions,
+                        :treesplits => splits,
+                        :data => dataDict)
+
+    else
+
+        features = table(reshape(dataDict[:features], length(dataDict[:features]),1))
+        targets = dataDict[:targets]
+
+        #Make the tree container
+        Tree = @MLJ.load DecisionTreeRegressor pkg=DecisionTree verbosity=0;
+        tree = Tree()
+
+        #tuning the min_purity_increase hyperparameter
+        r1 = range(tree, :merge_purity_threshold, lower=0.001, upper=1.0, scale=:log);
+        self_tuning_tree = TunedModel(model=tree,
 							  resampling=CV(nfolds=6,rng=Random.GLOBAL_RNG,shuffle=true),
 							  tuning=Grid(resolution=10),
 							  range=r1,
 							  measure=mae);
 
-    #store the self tuning tree in a machine
-    mach = machine(self_tuning_tree, trainFeatures, trainTargets);
+        #store the self tuning tree in a machine
+        mach = machine(self_tuning_tree, features, targets);
 
-    #fit the model
-    MLJ.fit!(mach, verbosity=2)
+        #fit the model
+        MLJ.fit!(mach, verbosity=2)
 
-    #print_tree(tree)
+        #print_tree(tree)
 
-    ## apply model to make predictions
-    smartPredictions = MLJ.predict(mach,features)
+        ## apply model to make predictions
+        smartPredictions = MLJ.predict(mach,features)
 
-    ##find the splits for the tree
-    splits = []
-    searchFields(splits,:featval,fitted_params(mach)[2].tree.node)
+        ##find the splits for the tree
+        splits = []
+        searchFields(splits,:featval,fitted_params(mach)[2].tree.node)
 
-    #Calculate BARS
-    dumbPredictions = []
-    for i∈1:length(DataDict["targets"])
-        push!(dumbPredictions, DataDict["median value of target"])
+        #Calculate BARS
+        naivePredictions = []
+        for i∈1:length(dataDict[:targets])
+            push!(naivePredictions, dataDict[:mediantarget])
+        end
+
+        naiveMAE = calcMAE(dataDict[:targets],naivePredictions)
+
+        smartMAE = calcMAE(dataDict[:targets],smartPredictions)
+
+        #model performance
+        SS_Res = sum((targets-smartPredictions).^2)
+        SS_Tot = sum((targets.-mean(targets)).^2)
+        R² = 1-SS_Res/SS_Tot
+        #R² = report(mach).best_history_entry[3][1]
+
+        BARSDict = Dict(:R² => R²,
+                   :smartMAE => smartMAE,
+                   :naiveMAE => naiveMAE,
+                   :naivepredictions => naivePredictions,
+                   :smartpredictions => smartPredictions,
+                   :treesplits => splits,
+                   :data => dataDict)
+
     end
-
-    dumbMAE = calcMAE(DataDict["targets"],dumbPredictions)
-
-    smartMAE = calcMAE(DataDict["targets"],smartPredictions)
-
-    #model performance
-    SS_Res = sum((targets-smartPredictions).^2)
-    SS_Tot = sum((targets.-mean(targets)).^2)
-    R2 = 1-SS_Res/SS_Tot
-    #R2 = report(mach).best_history_entry[3][1]
-
-    BARSDict = Dict("R squared" => R2,
-                   "smart MAE" => smartMAE,
-                   "dumb MAE" => dumbMAE,
-                   "dumb predictions" => dumbPredictions,
-                   "smart predictions" => smartPredictions,
-                   "decision tree splits" => splits,
-                   "Data" => DataDict)
     
     return BARSDict
 
@@ -146,80 +225,125 @@ end
 
 function calcSelfBARS(selfBARSDict)
 
-    smartMAE = selfBARSDict["smart MAE"]
-    dumbMAE = selfBARSDict["dumb MAE"]
+    smartMAE = selfBARSDict[:smartMAE]
+    naiveMAE = selfBARSDict[:naiveMAE]
 
-    α = dumbMAE/smartMAE
-    x = 0
-    selfBARSDict["α"] = α
-    if smartMAE>dumbMAE
+    r₀ = naiveMAE/smartMAE
+
+    selfBARSDict[:r₀] = r₀
+    if smartMAE>naiveMAE
         BARS = NaN
     else
         BARS = 1.0
     end
 
-    selfBARSDict["selfBARS"] = BARS
-    selfBARSDict["x"] = x
+    selfBARSDict[:selfBARS] = BARS
 
     return selfBARSDict
 end
 
 function calcBARS(BARSDict,selfBARSDict, acceptance)
 
-    smartMAE = BARSDict["smart MAE"]
-    dumbMAE = BARSDict["dumb MAE"]
-    smartMAEself = selfBARSDict["smart MAE"]
-    dumbMAEself = selfBARSDict["dumb MAE"]
-    β=acceptance
-    rSquared = BARSDict["R squared"]
-    x = (smartMAE-smartMAEself)/(dumbMAE)
-    if rSquared < 0 || rSquared > 1
-        BARS = NaN
+    smartMAE = BARSDict[:smartMAE]
+    naiveMAE = BARSDict[:naiveMAE]
+    smartMAEself = selfBARSDict[:smartMAE]
+    α=acceptance 
+    R² = BARSDict[:R²]
+    r = (smartMAE)/(naiveMAE)
+    r₀= (smartMAEself)/(naiveMAE)
+    if R² < 0 || R² > 1
+        BARS = 0
     else
-        BARS = exp.(-(x/β)^2)
+        BARS = exp.(-((r-r₀)/α)^2)
     end
 
-    BARSDict["BARS"] = BARS
-    BARSDict["x"] = x
-    BARSDict["β"] = β
+    BARSDict[:BARS] = BARS
+    BARSDict[:r] = r
+    BARSDict[:r₀] = r₀
+    BARSDict[:α] = α
 
     return BARSDict
 end
 
 function plotTREE(BARSDict)
-    theBARSScore = string(round(BARSDict["BARS"],sigdigits=3))
-    rSquared = string(round(BARSDict["R squared"],sigdigits = 3))
-    regressionPlot = scatter(BARSDict["Data"]["test features"] ,
-    BARSDict["Data"]["test targets"] ,
+    theBARSScore = string(round(BARSDict[:BARS],sigdigits=3))
+    R² = string(round(BARSDict[:R²],sigdigits = 3))
+    regressionPlot = scatter(BARSDict[:data][:features] ,
+        BARSDict[:smartpredictions] ,
+        markercolor = :purple ,
+        markershape = :cross ,
+        legend = :outerbottom,
+        title = L"BARS= "*theBARSScore*", "*L"R^2= "*R²,
+        label = "Smart Predictions")
+    scatter!(BARSDict[:data][:features],
+        BARSDict[:naivepredictions] ,
+        markercolor = :black ,
+        markershape = :xcross ,
+        label = "Naive Predictions")
+    if length(BARSDict[:treesplits])>0
+        vline!(BARSDict[:treesplits],
+        label = "Decision Tree Splits",
+        linecolor = :purple)
+    end
+
+    xlabel!("Feature: "*string(BARSDict[:data][:featurename]))
+    ylabel!("Target: "*string(BARSDict[:data][:targetname]))
+
+    if haskey(BARSDict[:data],:trainfeatures)
+        scatter!(BARSDict[:data][:testfeatures] ,
+        BARSDict[:data][:testtargets] ,
+            makercolor = :red ,
+            markershape = :rect ,
+            label = "Testing Data")
+        scatter!(BARSDict[:data][:trainfeatures] ,
+            BARSDict[:data][:traintargets] ,
+            makercolor = :blue ,
+            markershape = :circ ,
+            label = "Training Data")
+    end
+
+    return regressionPlot
+end
+
+function plotTREE(BARSDict)
+    theBARSScore = string(round(BARSDict[:BARS],sigdigits=3))
+    R² = string(round(BARSDict[:R²],sigdigits = 3))
+    regressionPlot = scatter(BARSDict[:data][:features] ,
+    BARSDict[:data][:targets] ,
         makercolor = :red ,
         markershape = :rect ,
         label = "Testing Data",
         legend = :outerbottom,
-        title = L"BARS= "*theBARSScore*", "*L"R^2_{avg}= "*rSquared)
-    scatter!(BARSDict["Data"]["train features"] ,
-        BARSDict["Data"]["train targets"] ,
-        makercolor = :blue ,
-        markershape = :circ ,
-        label = "Training Data")
-    scatter!(BARSDict["Data"]["features"] ,
-        BARSDict["smart predictions"] ,
+        title = L"BARS= "*theBARSScore*", "*L"R^2= "*R²)
+    scatter!(BARSDict[:data][:features] ,
+        BARSDict[:smartpredictions] ,
         markercolor = :purple ,
         markershape = :cross ,
         label = "Smart Predictions")
-    scatter!(BARSDict["Data"]["features"],
-        BARSDict["dumb predictions"] ,
+    scatter!(BARSDict[:data][:features],
+        BARSDict[:naivepredictions] ,
         markercolor = :black ,
         markershape = :xcross ,
-        label = "Dumb Predictions")
-    if length(BARSDict["decision tree splits"])>0
-        vline!(BARSDict["decision tree splits"],
+        label = "Naive Predictions")
+    if length(BARSDict[:treesplits])>0
+        vline!(BARSDict[:treesplits],
         label = "Decision Tree Splits",
         linecolor = :purple)
     end
-    xlabel!("Feature: "*string(BARSDict["Data"]["feature name"]))
-    ylabel!("Target: "*string(BARSDict["Data"]["target name"]))
+    xlabel!("Feature: "*string(BARSDict[:data][:featurename]))
+    ylabel!("Target: "*string(BARSDict[:data][:targetname]))
 
     return regressionPlot
+end
+
+function bars(dataFrame)
+
+    dataDict = makeDataDict(dataFrame)
+
+    BARSDict = makeBARSDict(dataDict)
+
+    return BARSDict
+
 end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,23 @@
 using BARS
 using Test
+using DataFrames
 
 @testset "BARS.jl" begin
-    # Write your tests here.
+
+    α = .01
+    t₀=0
+    t = [t₀+i for i∈0:.01:π/2]
+    x(t)=sin.(t)
+    y(t)=t.^2
+    dfself = DataFrame(X=x(t),Y=x(t))
+    df = DataFrame(X=x(t),Y=y(t))
+
+    @testset "BARS Tree tests" begin
+        
+        @test typeof(bars(dfself)) == Dict{Symbol,Any}
+
+        @test typeof(bars(df)) == Dict{Symbol,Any}
+
+    end
+
 end
